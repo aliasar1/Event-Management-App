babd21ced8d52075e11aa6a6bbfe9a3a6ca565ca
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2023-03-24 00:39:43 +0500

    Added Splash Screen

--- lib/manager/color_manager.dart
+++ lib/manager/color_manager.dart
@@ -1,1 +1,6 @@
-class ColorManager {}
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primaryLightColor = Color(0xFFbeb8d5);
+  static const Color primaryColor = Color(0xFFdc0a0b);
+}
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../manager/color_manager.dart';
+import '../manager/strings_manager.dart';
+import '../widgets/custom_text.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: ColorManager.primaryLightColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.network(
+                'https://assets1.lottiefiles.com/temporary_files/n1DHEu.json'),
+            const SizedBox(
+              height: 12,
+            ),
+            Txt(
+              text: StringsManager.appName,
+              fontFamily: 'Nunito',
+              fontSize: 34,
+              letterSpacing: 2,
+              fontWeight: FontWeight.w900,
+              color: ColorManager.primaryColor,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
